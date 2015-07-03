f509aff015d1f810eab8b3880866cf90f39efe5f
itheark <itheark@gmail.com>
2015-07-03 21:19:55 +0530

    resolutins

--- app/src/main/java/com/greycodes/zerito/NewFriendActivity.java
+++ app/src/main/java/com/greycodes/zerito/NewFriendActivity.java
@@ -1,21 +1,15 @@
 package com.greycodes.zerito;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.greycodes.zerito.helper.SendRequestService;
 
 
 public class NewFriendActivity extends ActionBarActivity {
 TextView tv_mob,tv_pin;
-    ImageView add;
+
     String mob,pin;
     //http://ramsandroid4all.blogspot.in/2014/01/display-all-contacts-from-contacts.html
     @Override
@@ -25,28 +19,6 @@ TextView tv_mob,tv_pin;
 
         tv_mob= (TextView) findViewById(R.id.nf_phoneno);
         tv_pin= (TextView) findViewById(R.id.nf_pin);
-        add= (ImageView) findViewById(R.id.nf_add);
-
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mob= tv_mob.getText().toString();
-                pin= tv_pin.getText().toString();
-
-                if(mob.length()<5){
-                    Toast.makeText(getApplicationContext(),"Please enter a proper mobile number",Toast.LENGTH_LONG).show();
-                }else if (pin.length()!=4){
-                    //Toast.makeText(getApplicationContext(),"Mobile number must have 4 digits",Toast.LENGTH_LONG).show();
-
-                }else {
-                    Intent service= new Intent(NewFriendActivity.this, SendRequestService.class);
-                    service.putExtra("mob1",mob);
-                    service.putExtra("pin",pin);
-                    startService(service);
-
-                }
-            }
-        });
 
     }
 
